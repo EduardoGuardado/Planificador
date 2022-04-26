7ca3e53a86020fa5af5ede13738444fb62f7ecff
EduardoGuardado <harry11.grd@gmail.com>
2022-04-26 17:59:59 -0600

    Tercera edición: agregamos la vista del login y la documentación actualizada

---
+++ application/views/inicio/login.php
@@ -0,0 +1,23 @@
+<div class="row justify-content-md-center" style="padding-top: 10em;">
+    <div class="col-md-auto">
+        <form class="form" action="<?php echo base_url()?>index.php/InicioControlador/InicioSesion" method="post">
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+                    <h3>Iniciar sesión</h3>
+                    <div id="errors" class="alert alert-danger" role="alert" style="display:none;"></div>
+                    <div class="form-group">
+                        <label for="usuario">Usuario asignado:</label>
+                        <input type="text" class="form-control" id="usuario" name="usuario" placeholder="Ah" required="">
+                    </div>
+                    <div class="form-group">
+                        <label for="clave">Contraseña:</label>
+                        <input type="password" class="form-control" id="clave" name="clave" placeholder="contraseña" required="">
+                    </div>
+                </div>
+                <div class="col-md-8 offset-md-2 text-right">
+                    <button type="submit" class="btn btn-primary">Acceder</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
